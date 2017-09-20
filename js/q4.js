7ce407a766cf40d79026ec2b1a85252a96f670aa
Nur Hidayah <dayah@Seths-MacBook.local>
2017-09-21 00:47:11 +0800

    qn 5 done

--- js/q4.js
+++ js/q4.js
@@ -1,4 +1,4 @@
-//List all the friends above in reverse alphabetical order.
+ //List all the friends above in reverse alphabetical order.
 
  var friends = 'Moe,Larry,Curly,Jane,Emma,Elizabeth,Elinor,Mary,Darcy,Grey,Lydia,Harriet';
  var alphaFriends = friends.split(",")
