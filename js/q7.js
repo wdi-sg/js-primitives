865cf7b9160f7bbf07b615d1edeb766d93a0acc1
yuki sy <yuki@Yukis-MacBook-Pro-2.local>
2017-09-21 10:23:05 +0800

    answers

---
+++ js/q7.js
@@ -0,0 +1,27 @@
+var foods = [
+  'Popcorn',
+  'Potato chips',
+  'Shrimp',
+  'Chicken rice',
+  'Poutine',
+  'Tacos',
+  'Toast',
+  'French Toast',
+  'Crab',
+  'Pho',
+  'Lasagna',
+  'Brownie',
+  'Lobster',
+  'Donuts',
+  'Ice cream',
+  'Hamburger',
+  'Sushi',
+  'Chocolate',
+  'Pizza'
+];
+
+var donuts = foods.indexOf('Donuts')
+
+foods.splice(donuts,1)
+
+console.log(foods)
