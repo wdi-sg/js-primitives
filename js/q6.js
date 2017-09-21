095a7183756cb394b02d9d9a03f659e5ff1f62c7
G174 <31766585+G174@users.noreply.github.com>
2017-09-21 10:24:29 +0800

    js exercises

---
+++ js/q6.js
@@ -0,0 +1,28 @@
+var foods = [
+  'Popcorn',
+  'Potato chips',
+  'Shrimp',
+  'Chicken rice',
+  'Poutine',
+  'Tacos',
+  'Toast',
+  'French Toast',
+  'Crab',
+  'Pho',
+  'Lasagna',
+  'Brownie',
+  'Lobster',
+  'Donuts',
+  'Ice cream',
+  'Hamburger',
+  'Sushi',
+  'Chocolate',
+  'Pizza'
+];
+
+for (var i = 0; i<= foods.length - 1; i++) {
+  //console.log(foods[i]);
+if (foods[i] === 'Pho') {
+   console.log(i+1)
+}
+}   
