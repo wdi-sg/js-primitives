34eb08461c1958aa0e48bd6e0e95b6c9b1e968df
Nur Hidayah <dayah@Seths-MacBook.local>
2017-09-21 09:30:54 +0800

    q10 mean

--- js/q10.js
+++ js/q10.js
@@ -2,4 +2,11 @@ var ages = [83, 53, 37, 29, 60, 30, 66, 19, 59, 41, 9, 64, 19, 80, 24, 53, 70, 1
 var level = ages.sort()
 //console.log(level)
 
-//
+    var total = 0;
+    for(i=0; i<level.length; i++){
+        total += level[i];
+    }
+
+    var mean = total/2
+
+    console.log(mean)
